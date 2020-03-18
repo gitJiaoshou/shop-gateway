260f6e3d9ebe9cb8c01f77452bb40b5c6c4a2dac
yangkaifei <52356670@qq.com>
2020-03-18 17:14:33 +0800

    修改部署命令

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,11 @@
-docker rm `docker ps -a -q`
-docker rmi --force `docker images | grep group | awk '{print $3}'`
+docker rmi registry.cn-beijing.aliyuncs.com/yangkaifei/shop-gateway:1.0.0
+docker rmi shop-gateway:1.0.0
 ## 打包
 mvn clean package -Dmaven.test.skip=true -U
 ## 构建docker镜像
 docker build -t shop-gateway:1.0.0 .
 #登录镜像
 sudo docker login --username=13730404063 registry.cn-beijing.aliyuncs.com
-#推送   [ImageId]根据实际情况修改
-sudo docker tag 6f8fafed9aa8 registry.cn-beijing.aliyuncs.com/yangkaifei/shop-gateway:1.0.0
+#推送
+sudo docker tag $(docker images |grep "shop-gateway" | awk '{print $3}') registry.cn-beijing.aliyuncs.com/yangkaifei/shop-gateway:1.0.0
 sudo docker push registry.cn-beijing.aliyuncs.com/yangkaifei/shop-gateway:1.0.0
--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,15 @@
-docker stop shop-gateway1.0.0 && docker rm shop-gateway1.0.0
+sudo docker stop shop-gateway1.0.0 && sudo docker rm shop-gateway1.0.0
 
-docker rmi $(docker images -q)
+sudo docker rmi $(sudo docker images -q)
 
-docker run --privileged=true --net=host \
+sudo docker run --privileged=true --net=host \
            -e SHOP_NACOS_ADDRESS=${SHOP_NACOS_ADDRESS} \
            -e SHOP_NACOS_NAMESPACE=${SHOP_NACOS_NAMESPACE} \
            -e SHOP_NACOS_CLUSTER_NAME=${SHOP_NACOS_CLUSTER_NAME} \
+           -e PUBLIC_IP=${PUBLIC_IP} \
            -e PROFILE_ACTIVE=${PROFILE_ACTIVE} \
            -v /shop/logs:/shop/logs \
            -d --name shop-gateway1.0.0 \
            registry.cn-beijing.aliyuncs.com/yangkaifei/shop-gateway:1.0.0
 
-docker logs -f shop-gateway1.0.0
+sudo docker logs -f shop-gateway1.0.0
